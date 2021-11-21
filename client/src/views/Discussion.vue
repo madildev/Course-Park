00dd85ba83ee1f66a985c9e6580f0d2da33c8132
madildev <adilgangster12@gmail.com>
2021-11-21 08:36:20 +0500

    Created the Discussion Route

---
+++ client/src/views/Discussion.vue
@@ -0,0 +1,28 @@
+/* eslint-disable */ 
+<template>
+  
+  <div class="discussion">
+     <chat></chat>
+  </div>
+
+</template>
+
+<script>
+import chat from "../components/Chat.vue";
+
+export default {
+    name: "discussion",
+    data(){
+        return{
+
+        }
+    },
+    components:{
+      chat
+    }  
+}
+</script>
+
+<style>
+
+</style>
