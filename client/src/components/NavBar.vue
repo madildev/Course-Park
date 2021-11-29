c1ccb542625755ae2457a701ed3eb5564739bca2
imoizza <moizazhar@yahoo.com>
2021-11-29 15:47:31 +0500

    blah!

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div  class="NavBar">
+        <h1>this is hell</h1>
+    </div>
+    
+
+</template>
+
+<script>
+
+export default {
+    name:"NavBar",
+    data (){
+    return {
+    }
+    }
+}
+</script>
+
+<style scoped>
+
+.navbar {
+
+}
+
+
+</style>
